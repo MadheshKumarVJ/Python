0c03db6dbafd7a2b1c67eca15b3682864dee8580
Maddy <vjmadheshkumarofficial@gmail.com>
2022-08-28 12:12:12 +0530

    Add a if else example
    a simple  attendance
    code using if elif else

---
+++ FlowControl.py
@@ -0,0 +1,11 @@
+# if else if else
+
+attendance = False;
+print("Attendance")
+check = input("Enter Y or N:")
+if check=="Y" or check=="y":
+    print("present")
+elif check=="N" or check=="n":
+    print("absent")
+else:
+    print("Enter a valide input")

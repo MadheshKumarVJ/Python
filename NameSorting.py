f5ca42752a9747aba80e89bdb63107064cf0da5b
Maddy <vjmadheshkumarofficial@gmail.com>
2022-08-28 21:52:43 +0530

    Sort numbers and names using lambda

---
+++ NameSorting.py
@@ -0,0 +1,7 @@
+names =["Madhesh","Pradeep","Mano","geethu"]
+names.sort(key = lambda x : len(x))
+print(names)
+
+numbers=[98,34,56,123,456,34,22]
+numbers.sort(key=lambda x : x%10)
+print(numbers)

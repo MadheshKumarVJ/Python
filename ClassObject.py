ce57f99da0742a72c52f9036c757302d13b2f2ee
MadheshKumarVJ <maddy@DESKTOP-CRIJIA5.localdomain>
2022-08-30 11:08:26 +0530

    Create constructor to the class

--- ClassObject.py
+++ ClassObject.py
@@ -1,14 +1,18 @@
 #creating class
 class TestPressEmployee:
+    #constructor is called when the object is created
+    def __init__(self,name) :
+        self.name = name
+        print(self.name)
     def BussinessTeam(self):
         print("Bussiness Team")
     def DeveloperTeam(self):
         print("Developer")
 
 #creating object
-Madhesh = TestPressEmployee()
+Madhesh = TestPressEmployee("Madhesh")
 #refering values
 Madhesh.DeveloperTeam()
 
-Bhuvi =TestPressEmployee()
+Bhuvi =TestPressEmployee("Bhuvi")
 Bhuvi.BussinessTeam()

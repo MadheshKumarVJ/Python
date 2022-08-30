ae697a6f7dfd8d04ac69a4bfd83654c767886e1b
MadheshKumarVJ <maddy@DESKTOP-CRIJIA5.localdomain>
2022-08-30 10:56:53 +0530

    Creat Class and Object

---
+++ ClassObject.py
@@ -0,0 +1,14 @@
+#creating class
+class TestPressEmployee:
+    def BussinessTeam(self):
+        print("Bussiness Team")
+    def DeveloperTeam(self):
+        print("Developer")
+
+#creating object
+Madhesh = TestPressEmployee()
+#refering values
+Madhesh.DeveloperTeam()
+
+Bhuvi =TestPressEmployee()
+Bhuvi.BussinessTeam()

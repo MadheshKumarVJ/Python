15cbf29e19f31d208980b82e5d851536312a8adc
MadheshKumarVJ <maddy@DESKTOP-CRIJIA5.localdomain>
2022-09-01 14:41:50 +0530

    Show how Iterator works

---
+++ Iterator.py
@@ -0,0 +1,20 @@
+
+list =[1,2,3,4]
+#to iter object both are valide 
+Iterator = iter(list)
+methodIte = list.__iter__()
+
+#using next method or __next__()
+print(next(methodIte))
+print(methodIte.__next__())
+print(next(methodIte))
+print(methodIte.__next__())
+
+#using next method or __next__()
+print(next(Iterator))
+print(Iterator.__next__())
+print(next(Iterator))
+print(Iterator.__next__())
+
+#this throws a error because there is no such method exist in list 
+print(list.__next__())

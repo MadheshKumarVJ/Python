a9dbdd045d59c9e85d855f2936ba141e090568cc
MadheshKumarVJ <maddy@DESKTOP-CRIJIA5.localdomain>
2022-09-02 10:53:00 +0530

    Shedule your weekday plans
    Update iterator to generator

--- WeekPlans.py
+++ WeekPlans.py
@@ -4,28 +4,35 @@ class Plans:
         Plans
     DESCRIPTION:
         Shedules your plans for the week days
-        this module simply uses (init,iter,next)
+        this module simply uses (init,getPlan(Generator))
         overrides this methods
     METHODS:
-        next(Object)
-        iter(Object)
-        PrintPlans(Object)
+        Object.getPlan()
+        Object.PrintPlans()
     """
     def __init__(self):
         self.max=4
         self.week =["Monday","Tuesday","Wednesday","Thursday","Friday"]
         self.plans=[]
-    def __iter__(self):
-        self.count =0
-        return self
-    def __next__(self):
-        if (self.count<=self.max):
+    #using generator (get rid of iter and next)
+    def getPlans(self):
+        self.count = 0
+        while (self.count<=self.max):
             plan = input("What is your plan for {} : ".format(self.week[self.count]))
             self.plans.append(plan)
             self.count+=1
-            return plan
-        else:
-            raise StopIteration
+            yield plan
+    # def __iter__(self):
+    #     self.count =0
+    #     return self
+    # def __next__(self):
+    #     if (self.count<=self.max):
+    #         plan = input("What is your plan for {} : ".format(self.week[self.count]))
+    #         self.plans.append(plan)
+    #         self.count+=1
+    #         return plan
+    #     else:
+    #         raise StopIteration
     def printPlan(self):
         for i in range(self.max+1):
             print("your plan for {} is to {}".format(self.week[i],self.plans[i]))
@@ -33,7 +40,7 @@ class Plans:
         print(Plans.__doc__)
 Madhesh_plans = Plans()
 Madhesh_plans.doc()
-planIterator = iter(Madhesh_plans)
-for i in range(5):
-    print(next(Madhesh_plans))
+#planIterator = iter(Madhesh_plans)
+for plan in Madhesh_plans.getPlans():
+    print(plan)
 Madhesh_plans.printPlan()

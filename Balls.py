ccc48c1fefdd21cb9233308ab63a96619e97cd86
MadheshKumarVJ <maddy@DESKTOP-CRIJIA5.localdomain>
2022-09-08 14:55:19 +0530

    Show how to use group()

--- Balls.py
+++ Balls.py
@@ -2,12 +2,14 @@ import re
 #balls in string
 balls ="red Green blue orange green white green yellow red green red green blue orange green white green yellow red green red green blue orange green white green yellow red green"
 
-#search pattern a for green ball
-search_pattern_of_green_ball="green"
-
+#search pattern a for green ball and yellow ball
+search_pattern_of_green_ball=r"(\bgreen+\b).+(\byellow+)"
 #finding all green balls position and storing it in a object
 green_balls= re.search(search_pattern_of_green_ball,balls,flags=re.I)
 
-#when you iterate it you will show all the positions
-#It can only say the first match
-print(green_balls)
+#shows the result if they exist
+print(green_balls.groups())
+#first group
+print(green_balls.group(1))
+#second group
+print(green_balls.group(2))

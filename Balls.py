c04fc93ab1c4f63539861d4caf0ebde870da16b7
MadheshKumarVJ <maddy@DESKTOP-CRIJIA5.localdomain>
2022-09-08 12:31:44 +0530

    Show how search works

--- Balls.py
+++ Balls.py
@@ -6,8 +6,8 @@ balls ="red Green blue orange green white green yellow red green red green blue 
 search_pattern_of_green_ball="green"
 
 #finding all green balls position and storing it in a object
-green_balls= re.finditer(search_pattern_of_green_ball,balls,flags=re.I)
+green_balls= re.search(search_pattern_of_green_ball,balls,flags=re.I)
 
 #when you iterate it you will show all the positions
-for green_ball in green_balls:
-    print(green_ball)
+#It can only say the first match
+print(green_balls)

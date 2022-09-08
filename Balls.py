f0b6c4a3b55696e22fb9eb7e5e0a78fcc15daf29
MadheshKumarVJ <maddy@DESKTOP-CRIJIA5.localdomain>
2022-09-08 11:24:18 +0530

    Add flag

--- Balls.py
+++ Balls.py
@@ -1,9 +1,9 @@
 import re
 #balls in string
-balls ="red green blue orange green white green yellow red green red green blue orange green white green yellow red green red green blue orange green white green yellow red green"
+balls ="red Green blue orange green white green yellow red green red green blue orange green white green yellow red green red green blue orange green white green yellow red green"
 #search pattern a for green ball
 search_pattern_of_green_ball="green"
 #finding all green balls and storing it in a list
-green_balls= re.findall(search_pattern_of_green_ball,balls)
+green_balls= re.findall(search_pattern_of_green_ball,balls,flags=re.I)
 #itha na kandu pidichathi
 print(green_balls)

cfc2549f5be81f1827c4ca66f52ef08dc6035060
MadheshKumarVJ <maddy@DESKTOP-CRIJIA5.localdomain>
2022-09-08 12:30:18 +0530

    Show how finditer works

--- Balls.py
+++ Balls.py
@@ -1,9 +1,13 @@
 import re
 #balls in string
 balls ="red Green blue orange green white green yellow red green red green blue orange green white green yellow red green red green blue orange green white green yellow red green"
+
 #search pattern a for green ball
 search_pattern_of_green_ball="green"
-#finding all green balls and storing it in a list
-green_balls= re.findall(search_pattern_of_green_ball,balls,flags=re.I)
-#itha na kandu pidichathi
-print(green_balls)
+
+#finding all green balls position and storing it in a object
+green_balls= re.finditer(search_pattern_of_green_ball,balls,flags=re.I)
+
+#when you iterate it you will show all the positions
+for green_ball in green_balls:
+    print(green_ball)
